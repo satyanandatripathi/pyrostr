b823af56d72a7e9685ce13ecd63546e86c5aae0f
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-26 14:32:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,7 @@ from pyromod import listen
 API_ID=os.environ.get("API_ID", None)
 API_HASH=os.environ.get("API_HASH", None)
 BOT_TOKEN=os.environ.get("BOT_TOKEN", None)
-APP_NAME= os.environ.get("APP_NAME", None)
-API_KEY= os.environ.get("API_KEY", None)
-HU_APP= from_key(API_KEY).apps()[APP_NAME]
+
 
 bot = Client(":memory:",
              api_id=API_ID,
