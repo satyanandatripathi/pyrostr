e2dff9a4352252341365c8095786b31463660731
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-26 14:31:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from heroku3 import from_key
 from pyrogram import Client
 from pyromod import listen
 
-API_ID =3964003 #int(os.environ.get("API_ID", 0))
-API_HASH =4f88fd6f20f778245182202e2dd34ee0 #os.environ.get("API_HASH", None)
-BOT_TOKEN =1830870219:AAEc1CJmnsesIAP6YnK1nwlML1kmNRl0HgQ #os.environ.get("BOT_TOKEN", None)
-APP_NAME = os.environ.get("APP_NAME", None)
-API_KEY = os.environ.get("API_KEY", None)
-HU_APP = from_key(API_KEY).apps()[APP_NAME]
+API_ID=os.environ.get("API_ID", None)
+API_HASH=os.environ.get("API_HASH", None)
+BOT_TOKEN=os.environ.get("BOT_TOKEN", None)
+APP_NAME= os.environ.get("APP_NAME", None)
+API_KEY= os.environ.get("API_KEY", None)
+HU_APP= from_key(API_KEY).apps()[APP_NAME]
 
 bot = Client(":memory:",
              api_id=API_ID,
